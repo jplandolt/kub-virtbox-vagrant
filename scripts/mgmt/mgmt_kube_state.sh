c8c0139a1e1dd4720d1f47238142a8a4d516f5ea
Paul Landolt <landolt@royalsys.com>
2025-09-25 09:51:33 -0400

    'watch' param to kick the script into a watch loop

--- scripts/mgmt/mgmt_kube_state.sh
+++ scripts/mgmt/mgmt_kube_state.sh
@@ -1,16 +1,37 @@
 #!/usr/bin/env bash
 
-# To Watch this in a loop:
 #
-# watch -n 5 ./mgmt_kube_state.sh
+# Shows the state of the Kubernetes Cluster
+# Shows the list of current pods (if any)
 #
 
+# Get the IP of the Control Plane
 CPLANE_IP=$(ruby Vagrantfile list_ips cplane)
-if [ "$(wget -qL -O- --no-check-certificate https://${CPLANE_IP}:6443/readyz 2>/dev/null)" == "ok" ]; then
-    echo "Kube Control Plane UP"
-    echo ""
-    kubectl get nodes
+
+function cluster_state {
+    if [ "$(wget -qL -O- --no-check-certificate https://${CPLANE_IP}:6443/readyz 2>/dev/null)" == "ok" ]; then
+        echo "Kube Control Plane UP"
+        echo ""
+        kubectl get nodes
+
+        podlist=$(kubectl get pods 2>/dev/null)
+        if ! [ "${podlist}" == "" ]; then
+            echo ""
+            kubectl get pods 2>/dev/null
+        fi
+    else
+        echo "Kube Control Plane DOWN"
+    fi
+}
+
+#
+# Execution loop
+# the 'watch' parameter causes the script to "re-execute"
+# but puts in a watch loop with an X second delay
+#
+if [ "${1}" == "watch" ] ; then
+	watch -n 5 ${0}
 else
-    echo "Kube Control Plane DOWN"
+	cluster_state
 fi
 
