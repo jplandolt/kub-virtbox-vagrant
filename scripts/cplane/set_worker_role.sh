9cb86a7a69d924182d4369d89729d50935111e8f
Paul Landolt <landolt@royalsys.com>
2025-09-27 09:48:48 -0400

    reduce complication

--- scripts/cplane/set_worker_role.sh
+++ scripts/cplane/set_worker_role.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env bash
 
-echo "⚙️  Setting worker node(s) role to 'worker'"
+echo "⚙️  Setting worker node(s) > 'node-role.kubernetes.io/worker=worker'"
 
-# Get list of nodes, look through the workers
+# Get list of nodes, run through the workers
 kub_nodes_json=`kubectl get nodes -o json`
 for node_name in `echo ${kub_nodes_json} | jq -r '.items[] | select(.metadata.name | startswith("worker")) | .metadata.name'` ; do
-
-    # Get the current role (if any) set for the worker node
-    node_role=`echo ${kub_nodes_json} | jq -r '.items[] | select(.metadata.name == "${node_name}") | .metadata.labels | to_entries[] | select(.key | startswith("node-role.kubernetes.io/worker")) | .value'`
-
-    # Only set the role if it's not already in place
-    echo "🔍 node role for '${node_name}': '${node_role}'"
-
-    if [ ! "${node_role}" == "worker" ] ; then
-        echo "⚙️  set worker node role for '${node_name}'"
-        kubectl label --overwrite node ${node_name} node-role.kubernetes.io/worker=worker >/dev/null
-   else
-        echo "  role 'worker' already set for node '${node_name}'"
-   fi
+     echo "⚙️  set worker node role for '${node_name}'"
+     kubectl label --overwrite node ${node_name} node-role.kubernetes.io/worker=worker >/dev/null
 done
 
 echo ""
-echo "🔍 k8s nodes with roles:"
-echo "---------------------"
+echo "🔍 Cluster nodes and roles:"
+echo "---------------------------"
 kubectl get nodes
+echo ""
